b16994ef3f289b7a2b49bc938dacab29d686bff1
DELL <DELL@AHMEDPC>
2025-04-25 18:01:29 +0300

    Add project files.

---
+++ part1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("welcome");
+           Console.Write("what is your name :");
+           string name = Console.ReadLine();
+           Console.Write("what is your age :");
+           int age = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine($"your name is {name}, you {age} years old");
+        }
+    }
+}
---
+++ part2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("try create 2 project in solution");
+        }
+    }
+}
