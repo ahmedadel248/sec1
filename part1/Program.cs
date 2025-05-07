cf74c194c83cb50e38fea9a1a3524c0487a46353
Ahmed Adel <131630355+ahmedadel248@users.noreply.github.com>
2025-05-07 22:01:27 +0300

    Update Program.cs

--- part1/Program.cs
+++ part1/Program.cs
@@ -1,15 +1,24 @@
-﻿namespace part1
+using System.Numerics;
+
+namespace task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("welcome");
-           Console.Write("what is your name :");
-           string name = Console.ReadLine();
-           Console.Write("what is your age :");
-           int age = Convert.ToInt32(Console.ReadLine());
-           Console.WriteLine($"your name is {name}, you {age} years old");
+            Console.WriteLine("welcome to BR's Carpet Cleaning Service " +
+                "\nCharges:\n$25 per small \n$35 per large \nSales tax rate is 6%" +
+                "\nEstimates are valid for 30 days");   
+            
+            Console.Write("Enter the number of small carpets : ");
+            int S = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of Large carpets : ");
+            int L = Convert.ToInt32(Console.ReadLine());
+
+            double Total = (L * 35 + S * 25);
+            Total += (Total * 0.06);
+
+            Console.WriteLine($"Total estimate:{Total}\r\nThis estimate is valid for 30 days");
         }
     }
 }
